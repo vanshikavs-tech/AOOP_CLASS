eada4f8081c6c14c6191d9525304bb3ad6cb5c9e
Vanshika Singh <vanshikanr06@gmail.com>
2026-08-02 19:00:00 +0530

    Add multiplication program

---
+++ UNIT-1/multiplication.java
@@ -0,0 +1,10 @@
+public static void main(String[] args) {
+    int a = 5;
+    int i = 0;
+    int m = 0;
+
+    for (i = 1; i <= 10; i++) { 
+        m = i * a;
+        System.out.println(a + " * " + i + " = " + m);
+    }
+}
